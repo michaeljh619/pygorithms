31c10bc8a6579d3a1bcf26cbfed7897a3deb42fb
Michael Hart <michaeljh619@yahoo.com>
2019-01-20 15:06:03 -0800

    feat: add bubble sort algorithm

---
+++ bubble-sort/bubble_sort.py
@@ -0,0 +1,33 @@
+def bubble_sort(unsorted_list, smallest_first=True):
+    # make shallow copy of list
+    sorted_list = unsorted_list[:]
+
+    while True:
+        was_swapped = False
+
+        # begin passes at first index
+        for pass_start_index in range(len(sorted_list)-1):
+
+            # individual pass
+            for i in range(pass_start_index, len(sorted_list)-1):
+            
+                # compare and set flag if needs to be swapped
+                needs_swap = False
+                if smallest_first and sorted_list[i] > sorted_list[i+1]:
+                    needs_swap = True
+                elif not smallest_first and sorted_list[i] < sorted_list[i+1]:
+                    needs_swap = True
+
+                # swap if necessary
+                if needs_swap:
+                    tmp = sorted_list[i]
+                    sorted_list[i] = sorted_list[i+1]
+                    sorted_list[i+1] = tmp
+                    was_swapped = True
+        
+        # stop passes if no swaps
+        if not was_swapped:
+            break
+
+    # return sorted list
+    return sorted_list
