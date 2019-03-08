eb1a77ca17f4b9fcb2d8a681b5b86679a1ee6515
Michael Hart <michaeljh619@yahoo.com>
2019-03-08 11:30:14 -0800

    feat: add best_path solution and tests

--- dynamic-prog/best_path.py
+++ dynamic-prog/best_path.py
@@ -1,3 +1,8 @@
+'''
+TODO: Write explanation to problem
+'''
+
+
 class BestPath:
     def __init__(self, grid):
         # grid vals
@@ -9,8 +14,75 @@ class BestPath:
         self.score_grid = self.__create_empty_grid()
         self.path_grid = self.__create_empty_grid()
 
-    def tabulate(self):
-        pass
+    def solve(self):
+        # start at top left and tabulate grid
+        for x in range(self.cols):
+            for y in range(self.rows):
+                # neighbor positions
+                top_x = x
+                top_y = y - 1
+                left_x = x - 1
+                left_y = y
+
+                # value of current cell
+                val = self.__get_item_from_grid(self.grid, x, y)
+
+                # best scores from neighbors
+                top_score = self.__get_item_from_grid(self.score_grid,
+                                                      top_x, top_y)
+                left_score = self.__get_item_from_grid(self.score_grid,
+                                                       left_x, left_y)
+
+                # best paths from neighbors
+                top_path = self.__get_item_from_grid(self.path_grid,
+                                                     top_x, top_y)
+                left_path = self.__get_item_from_grid(self.path_grid,
+                                                      left_x, left_y)
+
+                # calculate best path and best score to this position
+                best_path = ""
+                best_score = val
+
+                # determine which prev neighbor to use
+                use_left_neighbor = False
+                use_top_neighbor = False
+                # top left position, no prev neighbors
+                if top_score is None and left_score is None:
+                    pass
+                # on top edge
+                elif top_score is None:
+                    use_left_neighbor = True
+                # on left edge
+                elif left_score is None:
+                    use_top_neighbor = True
+                # two prev neighbors, pick which is better
+                else:
+                    if left_score > top_score:
+                        use_left_neighbor = True
+                    else:
+                        use_top_neighbor = True
+
+                # get best path and score based on which was better
+                if use_left_neighbor:
+                    best_path = left_path + "R"
+                    best_score = left_score + val
+                elif use_top_neighbor:
+                    best_path = top_path + "D"
+                    best_score = top_score + val
+
+                # set best path and score
+                self.score_grid[y][x] = best_score
+                self.path_grid[y][x] = best_path
+
+    def get_best_score(self):
+        if self.rows == 0 or self.cols == 0:
+            return 0
+        return self.score_grid[self.rows-1][self.cols-1]
+
+    def get_best_path(self):
+        if self.rows == 0 or self.cols == 0:
+            return ""
+        return self.path_grid[self.rows-1][self.cols-1]
 
     def __create_empty_grid(self):
         new_grid = []
@@ -20,3 +92,13 @@ class BestPath:
                 row.append(None)
             new_grid.append(row)
         return new_grid
+
+    def __get_item_from_grid(self, grid, col, row):
+        # determine if out of bounds
+        if col < 0 or row < 0:
+            return None
+        if col >= len(grid[0]) or row >= len(grid):
+            return None
+
+        # get item
+        return grid[row][col]
--- dynamic-prog/test_best_path.py
+++ dynamic-prog/test_best_path.py
@@ -8,10 +8,21 @@ def pytest_generate_tests(metafunc):
         [[]],
         [[1]],
         [[0, 2], [1, 0]],
-        [[0, 2, 3], [1, 4, 0]]
+        [[0, 2, 3], [1, 4, 0]],
+        [[0, 2, 3], [1, 4, 0], [5, 12, 0]],
     ]
     if 'grid' in metafunc.fixturenames:
         metafunc.parametrize('grid', grids)
+
+    grid_score_paths = [
+        (grids[0], 0, ""),
+        (grids[1], 1, ""),
+        (grids[2], 2, "RD"),
+        (grids[3], 6, "RDR"),
+        (grids[4], 18, "RDDR"),
+    ]
+    if 'grid_score_path' in metafunc.fixturenames:
+        metafunc.parametrize('grid_score_path', grid_score_paths)
 
 
 def test_constructor(grid):
@@ -27,3 +38,39 @@ def test_create_empty_grid(grid):
     empty_grid = bp._BestPath__create_empty_grid()
     assert len(empty_grid) == len(grid)
     assert len(empty_grid[0]) == len(grid[0])
+
+
+def test_get_item_from_grid(grid):
+    # create obj
+    bp = BestPath(grid)
+
+    # loop from -1 to 1 over end of length of grid
+    for col in range(-1, len(grid)+1):
+        for row in range(-1, len(grid[0])+1):
+            # getting item from outside of grid
+            should_be_none = False
+            if col < 0 or col >= len(grid[0]):
+                should_be_none = True
+            elif row < 0 or row >= len(grid):
+                should_be_none = True
+
+            if should_be_none:
+                assert bp._BestPath__get_item_from_grid(grid,
+                                                        col,
+                                                        row) is None
+            else:
+                assert bp._BestPath__get_item_from_grid(grid,
+                                                        col,
+                                                        row) == grid[row][col]
+
+
+def test_solution(grid_score_path):
+    grid = grid_score_path[0]
+    score = grid_score_path[1]
+    path = grid_score_path[2]
+
+    bp = BestPath(grid)
+    bp.solve()
+
+    assert bp.get_best_score() == score
+    assert bp.get_best_path() == path
